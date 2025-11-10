62c1888d81b85d0029a1230623c977361254d7a4
Sourav Kumar Bhagat <78733805+srv184@users.noreply.github.com>
2025-11-10 13:31:00 +0530

    Refactor auth controller for improved readability

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -3,112 +3,129 @@ import UserModel from "../models/userModel.js";
 import { BadRequestError, UnauthorizedError } from "../errors/index.js";
 import attachCookie from "../utils/attachCookies.js";
 
+// --------------------- REGISTER ---------------------
 const register = async (req, res, next) => {
-  const { firstName, lastName, email, password } = req.body;
-  if (!firstName || !lastName || !email || !password) {
-    throw new BadRequestError(
-      "Please provide your first and last name, email and password"
-    );
-  }
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    if (!firstName || !lastName || !email || !password) {
+      throw new BadRequestError(
+        "Please provide your first and last name, email and password"
+      );
+    }
 
-  // Checking for the duplicate email
-  const userAlreadyExists = await UserModel.findOne({ email });
-  if (userAlreadyExists) {
-    throw new BadRequestError("This email is already in use");
-  }
+    // Check for duplicate email
+    const userAlreadyExists = await UserModel.findOne({ email });
+    if (userAlreadyExists) {
+      throw new BadRequestError("This email is already in use");
+    }
 
-  const user = await UserModel.create(req.body);
+    const user = await UserModel.create(req.body);
 
-  const token = user.createJWT();
-  attachCookie({ res, token });
+    const token = user.createJWT();
+    attachCookie({ res, token }); // sends cookie to frontend
 
-  res.status(StatusCodes.CREATED).json({
-    status: "success",
-    // token,
-    user: {
-      firstName: user.firstName,
-      lastName: user.lastName,
+    res.status(StatusCodes.CREATED).json({
+      status: "success",
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userLocation: user.userLocation,
+        email: user.email,
+      },
       userLocation: user.userLocation,
-      email: user.email,
-    },
-    userLocation: user.userLocation,
-  });
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+// --------------------- LOGIN ---------------------
 const login = async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    throw new BadRequestError("Please provide your email and password");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new BadRequestError("Please provide your email and password");
+    }
+
+    const user = await UserModel.findOne({ email }).select("+password");
+    if (!user) {
+      throw new UnauthorizedError("Invalid credentials!");
+    }
+
+    const isPasswordCorrect = await user.comparePasswords(password);
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedError("Invalid credentials!");
+    }
+
+    const token = user.createJWT();
+    user.password = undefined; // hide password
+
+    attachCookie({ res, token });
+
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      user,
+      userLocation: user.userLocation,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const user = await UserModel.findOne({ email }).select("+password");
-  if (!user) {
-    throw new UnauthorizedError("Authentication failed!");
-  }
-
-  // Comparing the user's original and current password
-  const passwordIsCorrect = await user.comparePasswords(password);
-  if (!passwordIsCorrect) {
-    throw new UnauthorizedError("Incorrect password!");
-  }
-
-  const token = user.createJWT();
-  user.password = undefined;
-
-  attachCookie({ res, token });
-
-  res.status(StatusCodes.OK).json({
-    status: "success",
-    // token,
-    user,
-    userLocation: user.userLocation,
-  });
 };
 
+// --------------------- UPDATE USER ---------------------
 const updateUser = async (req, res, next) => {
-  const { firstName, lastName, userLocation, email } = req.body;
+  try {
+    const { firstName, lastName, userLocation, email } = req.body;
+    if (!firstName || !lastName || !email) {
+      throw new BadRequestError(
+        "Please provide your first and last name, email"
+      );
+    }
 
-  if (!firstName || !lastName || !email) {
-    throw new BadRequestError(
-      "Please provide your first and last name, email and password"
-    );
+    const user = await UserModel.findOne({ _id: req.user.userId });
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.userLocation = userLocation;
+    user.email = email;
+
+    await user.save();
+
+    const token = user.createJWT();
+    attachCookie({ res, token });
+
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      user,
+      userLocation: user.userLocation,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const user = await UserModel.findOne({ _id: req.user.userId });
-
-  user.firstName = firstName;
-  user.lastName = lastName;
-  user.userLocation = userLocation;
-  user.email = email;
-
-  await user.save();
-
-  const token = user.createJWT();
-  attachCookie({ res, token });
-
-  res.status(StatusCodes.OK).json({
-    status: "success",
-    // token,
-    user,
-    userLocation: user.userLocation,
-  });
 };
 
-// Getting the current user to prevent losing his token on the page reload
-const getCurrentUser = async (req, res) => {
-  const user = await UserModel.findOne({ _id: req.user.userId });
+// --------------------- GET CURRENT USER ---------------------
+const getCurrentUser = async (req, res, next) => {
+  try {
+    const user = await UserModel.findOne({ _id: req.user.userId });
 
-  res.status(StatusCodes.OK).json({
-    status: "success",
-    user,
-    userLocation: user.userLocation,
-  });
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      user,
+      userLocation: user.userLocation,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+// --------------------- LOGOUT ---------------------
 const logout = async (req, res) => {
   res.cookie("token", "logout", {
     httpOnly: true,
     expires: new Date(Date.now()),
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
   });
 
   res.status(StatusCodes.OK).json({ message: "User logged out!" });
