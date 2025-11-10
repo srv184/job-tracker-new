d60d5089fcd0496d27cf481c0b77ef3e876d4903
Sourav Kumar Bhagat <78733805+srv184@users.noreply.github.com>
2025-11-10 13:29:27 +0530

    Refactor auth routes to use controller functions

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,27 +1,38 @@
 import express from "express";
 import rateLimiter from "express-rate-limit";
 import authVerification from "../middleware/auth.js";
+import testUser from "../middleware/testUser.js";
 import {
-  register,
-  login,
+  register as registerController,
+  login as loginController,
   updateUser,
   getCurrentUser,
-  logout,
+  logout as logoutController,
 } from "../controllers/auth-controllers.js";
-import testUser from "../middleware/testUser.js";
 
 const router = express.Router();
+
+// Rate limiter
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP requests per windowMS
+  max: 100,
   message:
     "Too many requests from this IP address. Please try again in 15 minutes",
 });
 
-router.route("/register").post(apiLimiter, register);
-router.route("/login").post(apiLimiter, login);
-router.route("/updateUser").patch(authVerification, testUser, updateUser);
-router.route("/getCurrentUser").get(authVerification, getCurrentUser);
-router.route("/logout").get(logout);
+// --------------------- REGISTER ---------------------
+router.post("/register", apiLimiter, registerController);
+
+// --------------------- LOGIN ---------------------
+router.post("/login", apiLimiter, loginController);
+
+// --------------------- UPDATE USER ---------------------
+router.patch("/updateUser", authVerification, testUser, updateUser);
+
+// --------------------- GET CURRENT USER ---------------------
+router.get("/getCurrentUser", authVerification, getCurrentUser);
+
+// --------------------- LOGOUT ---------------------
+router.get("/logout", logoutController);
 
 export default router;
